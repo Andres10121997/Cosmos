01744fde1c7dd969344cb5619e01583927a7e35f
Andres10121997 <48809220+Andres10121997@users.noreply.github.com>
2022-07-12 20:52:16 -0400

    Update Scripts

--- src/Script/TypeScript/Artificial intelligence/Cellular automata/Insert.ts
+++ src/Script/TypeScript/Artificial intelligence/Cellular automata/Insert.ts
@@ -2,50 +2,58 @@
  * @function
  * @name InsertCellularAutomata
  * @param {string} CanvasID
- * @returns {void}
+ * @returns {Promise<void>}
  */
-function InsertCellularAutomata(CanvasID: string): void
+async function InsertCellularAutomata(CanvasID: string): Promise<void>
 {
-    // Constants.
-    /**
-     * @access private
-     * @member
-     * @constant
-     * @type {HTMLCanvasElement}
-     * @alias Canvas
-     */
-    const Canvas: HTMLCanvasElement = (document.getElementById(CanvasID) as HTMLCanvasElement);
+    await new Promise((): void => {
+        // Constants.
+        /**
+         * @access private
+         * @member
+         * @constant
+         * @type {HTMLCanvasElement}
+         * @alias Canvas
+         */
+        const Canvas: HTMLCanvasElement = (document.getElementById(CanvasID) as HTMLCanvasElement);
 
-    /**
-     * @access private
-     * @member
-     * @constant
-     * @type {number}
-     * @alias Size
-     */
-    const Size: number = Canvas.clientHeight;
-    
-    /**
-     * @access private
-     * @member
-     * @constant
-     * @type {CanvasRenderingContext2D | null}
-     * @name ctx
-     */
-    const ctx: (CanvasRenderingContext2D | null) = Canvas.getContext(`2d`);
+        /**
+         * @access private
+         * @member
+         * @constant
+         * @type {number}
+         * @alias Size
+         */
+        const Size: number = Canvas.clientHeight;
+        
+        /**
+         * @access private
+         * @member
+         * @constant
+         * @type {CanvasRenderingContext2D | null}
+         * @name ctx
+         */
+        const ctx: (CanvasRenderingContext2D | null) = Canvas.getContext(`2d`);
 
 
 
-    // Objects.
-    /**
-     * @access private
-     * @member
-     * @constant
-     * @type {CellularAutomata}
-     * @name cellularAutomata
-     */
-    const cellularAutomata: CellularAutomata = new CellularAutomata(Size, ctx);
+        // Objects.
+        /**
+         * @access private
+         * @member
+         * @constant
+         * @type {CellularAutomata}
+         * @name cellularAutomata
+         */
+        const cellularAutomata: CellularAutomata = new CellularAutomata(Size, ctx);
 
-    cellularAutomata.Create();
-    setInterval((): void => cellularAutomata.Next(), 1000);
+        cellularAutomata.Create();
+        setInterval((): void => cellularAutomata.Next(), 1000);
+    })
+    .catch((reason: any): void => {
+        console.error(`---`);
+        console.error(`Error InsertCellularAutomata function.`);
+        console.error(`Error: ${reason}`);
+        console.error(`---`);
+    })
 }
--- src/Script/TypeScript/index.ts
+++ src/Script/TypeScript/index.ts
@@ -1,17 +1,35 @@
+/**
+ * Maths.
+ */
 import "./Maths/Fibonacci.js";
-import "./Maths/Calculation/Age.js";
-import "./Maths/Calculation/Calculate.js";
 
+// Calculation.
+import "./Maths/Calculation/Calculate.js";
+import "./Maths/Calculation/Age.js";
+
+/**
+ * Time.
+ */
 import "./Time/Today.js";
 import "./Time/Variable/Date.js";
+// Clock.
 import "./Time/Clock/Clock.js";
 import "./Time/Clock/Insert/Date.js";
 import "./Time/Clock/Insert/Time.js";
 
+/**
+ *  Validation.
+ */
 import "./Validation/Validation.js";
 import "./Validation/Form.js";
 
+/**
+ * Variables.
+ */
 import "./Variable/Form.js";
 import "./Variable/Regular Expression.js";
 
+/**
+ * WYSIWYG.
+ */
 import "./WYSIWYG/Rich text editor.js";
