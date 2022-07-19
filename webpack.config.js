4ebf3cdfa16191a4196544ab50a78ddfe0c1abdd
Andres10121997 <48809220+Andres10121997@users.noreply.github.com>
2022-07-19 13:15:57 -0400

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
 const basePath = __dirname;
 const distPath = "dist";
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     // mode.
-    mode: "development",
+    mode: "production", // "production" | "development" | "none"
     // entry point.
     entry: {
         app: [
@@ -16,14 +17,37 @@ module.exports = {
         rules: [
             {
                 test: /\.js/,
-                exclude: /node_modules/,
-                use: ["babel-loader"],
+                exclude: [
+                    /node_modules/,
+                    path.join(basePath, "node_modules"),
+                    path.join(basePath, "dist")
+                ],
+                use: [
+                    "babel-loader"
+                ],
+                type: "javascript/auto",
             }
         ]
     },
     // output point.
     output: {
         path: path.join(basePath, distPath),
-        filename: "Cosmos.js"
+        filename: "JavaScript/Cosmos.js"
+    },
+    // plugins
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./View/Clock.html"
+        })
+    ],
+    optimization: {
+        chunkIds: "size",
+        // method of generating ids for chunks
+        moduleIds: "size",
+        // method of generating ids for modules
+        mangleExports: "size",
+        // rename export names to shorter names
+        minimize: false,
+        // minimize the output files
     }
 };
